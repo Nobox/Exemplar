b3d05638587e854c72deb9baf5d1aaeacbee28d2
Jonathan Torres <jonathan@nobox.com>
2015-10-06 11:24:46 -0400

    Fixing layout.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Mini trivia</title>
+        <title>Trivia</title>
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
@@ -18,9 +18,7 @@
     </head>
     <body>
         <div class="container">
-            <div class="row">
-                @yield('content')
-            </div>
+            @yield('content')
         </div>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
