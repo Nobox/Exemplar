eb78e9e7f76674c2cabd716c541a0416aabb351c
Jonathan Torres <jonathan@nobox.com>
2015-10-06 12:29:54 -0400

    Gulp and vue setup.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,5 +23,7 @@
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/vue/0.12.16/vue.min.js"></script>
+        <script src="js/app.js"></script>
     </body>
 </html>
