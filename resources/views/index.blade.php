e61677b7d01699d696974d1adbb4b50d03671885
Jonathan Torres <jonathan@nobox.com>
2015-10-06 11:56:05 -0400

    Basic view with questions.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,42 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="row">
+    <div class="row hidden">
         <div class="col-md-12">
             <h1>Trivia</h1>
+            <a href="#" class="btn btn-primary">Start here</a>
+        </div>
+    </div>
+    <div class="row hidden">
+        <div class="col-md-12">
+            <h1>Question 1?</h1>
+            <ul class="list-unstyled">
+                <li>
+                    <div class="checkbox">
+                        <label><input type="checkbox"> Choice 1</label>
+                    </div>
+                </li>
+                <li>
+                    <div class="checkbox">
+                        <label><input type="checkbox"> Choice 1</label>
+                    </div>
+                </li>
+                <li>
+                    <div class="checkbox">
+                        <label><input type="checkbox"> Choice 1</label>
+                    </div>
+                </li>
+                <li>
+                    <div class="checkbox">
+                        <label><input type="checkbox"> Choice 1</label>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="row hidden">
+        <div class="col-md-12">
+            <h1>Congrats, you finished!</h1>
         </div>
     </div>
 @stop
