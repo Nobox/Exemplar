347a8c72e7caf7f9c1c1e12396e34dba946f700a
Jonathan Torres <jonathan@nobox.com>
2015-10-08 10:23:29 -0400

    Updates on trivia component, add questions data.

--- resources/assets/js/components/trivia.js
+++ resources/assets/js/components/trivia.js
@@ -1,5 +1,21 @@
 var trivia = {
-    template: require('./trivia.template.html')
+    template: require('./trivia.template.html'),
+    components: {}
 };
+
+/**
+ * Data for trivia
+ * @return {object}
+ */
+trivia.data = function () {
+    return {
+        questions: require('../data/questions')
+    };
+}
+
+/**
+ * Trivia child components
+ */
+trivia.components['question'] = require('./question')
 
 module.exports = trivia;
---
+++ resources/assets/js/data/questions.js
@@ -0,0 +1,26 @@
+var questions = [
+    {
+        question: 'What is your name?',
+        correct: 1,
+        options: [
+            'Charles',
+            'Rambo',
+            'Beep',
+            'Foo',
+            'Anakin'
+        ]
+    },
+    {
+        question: 'What is your location?',
+        correct: 2,
+        options: [
+            'Mordor',
+            'Mount Olimpus',
+            'The Jungle',
+            'Miami',
+            'US'
+        ]
+    }
+];
+
+module.exports = questions;
