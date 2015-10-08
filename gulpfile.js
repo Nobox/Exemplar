75e9acec4214a197ec9ad769d76750c168945757
Jonathan Torres <jonathan@nobox.com>
2015-10-08 11:36:29 -0400

    Enable watchers on gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
-var elixir = require('laravel-elixir');
+var elixir = require('laravel-elixir'),
+    config = elixir.config;
+
+// Enable watchify polling for our NFS-mounted VMs.
+config.js.browserify.watchify.options.poll = true;
 
 elixir(function (mix) {
     mix.browserify('app.js');
