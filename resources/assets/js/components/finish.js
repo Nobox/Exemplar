008154856df2443a25480be31e0a4f78cd178ed8
Jonathan Torres <jonathan@nobox.com>
2015-10-09 16:05:21 -0400

    Display trivia results.

--- resources/assets/js/components/finish.js
+++ resources/assets/js/components/finish.js
@@ -2,4 +2,15 @@ var finish = {
     template: require('./finish.template.html')
 };
 
+/**
+ * Component data
+ * @return {object}
+ */
+finish.data = function () {
+    return {
+        correct: this.$root.results.correct,
+        incorrect: this.$root.results.incorrect
+    };
+}
+
 module.exports = finish;
